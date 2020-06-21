442344f74c8da214a802dd30d49d245f3c437279
Ian Johnson <ianprime0509@gmail.com>
2020-06-21 16:43:57 -0400

    Add more test cases for DrawTo

--- kanjivg/kanjivg_test.go
+++ kanjivg/kanjivg_test.go
@@ -154,6 +154,88 @@ func TestDrawTo(t *testing.T) {
 				Angle2: 2 * math.Pi,
 			}, Fill: true},
 		}},
+		{"M5,10c5,5 5,0 10 -5C8 2 6.7 8.5 2 5 6 10 15 20 20 15", false, []Drawing{
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				X:     5,
+				Y:     10,
+				X1:    10,
+				Y1:    15,
+				X2:    10,
+				Y2:    10,
+				X3:    15,
+				Y3:    5,
+			}, Fill: false},
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				X:     15,
+				Y:     5,
+				X1:    8,
+				Y1:    2,
+				X2:    6.7,
+				Y2:    8.5,
+				X3:    2,
+				Y3:    5,
+			}, Fill: false},
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				X:     2,
+				Y:     5,
+				X1:    6,
+				Y1:    10,
+				X2:    15,
+				Y2:    20,
+				X3:    20,
+				Y3:    15,
+			}, Fill: false},
+		}},
+		// The first stroke of kvg:kanji_057f6
+		{"m16.186819,29.104494c1.331878,0.571588,2.950798,0.368357,4.340084,0.254039,5.499736,-0.495376,15.592153,-2.095821,21.252632,-2.514985,1.722256,-0.12702,3.478956,-0.203231,5.178248,0.241337", true, []Drawing{
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				// c1.331878,0.571588,2.950798,0.368357,4.340084,0.254039
+				X:  16.186819,
+				Y:  29.104494,
+				X1: 17.518697,
+				Y1: 29.676082,
+				X2: 19.137617,
+				Y2: 29.472851,
+				X3: 20.526903,
+				Y3: 29.358533,
+			}, Fill: false},
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				// c5.499736,-0.495376,15.592153,-2.095821,21.252632,-2.514985
+				X:  20.526903,
+				Y:  29.358533,
+				X1: 26.026639,
+				Y1: 28.863157,
+				X2: 36.119056,
+				Y2: 27.262712,
+				X3: 41.779535,
+				Y3: 26.843548,
+			}, Fill: false},
+			{Shape: Curve{
+				Shape: Shape{LineWidth: 1},
+				// c1.722256,-0.12702,3.478956,-0.203231,5.178248,0.241337
+				X:  41.779535,
+				Y:  26.843548,
+				X1: 43.501791,
+				Y1: 26.716528,
+				X2: 45.258491,
+				Y2: 26.640317,
+				X3: 46.957783,
+				Y3: 27.084885,
+			}, Fill: false},
+			{Shape: Arc{
+				Shape:  Shape{LineWidth: 1, R: 1, G: 0, B: 0},
+				X:      16.186819,
+				Y:      29.104494,
+				R:      1.5,
+				Angle1: 0,
+				Angle2: 2 * math.Pi,
+			}, Fill: true},
+		}},
 	}
 
 	closeTo := func(x, y float64) bool {
@@ -166,7 +248,7 @@ func TestDrawTo(t *testing.T) {
 		test.stroke.DrawTo(c, test.markStart)
 
 		if diff := cmp.Diff(test.drawings, c.Drawings, cmp.Comparer(closeTo)); diff != "" {
-			t.Errorf("for stroke %q: %v", test.stroke, diff)
+			t.Errorf("for stroke %q (-want, +got):\n%v", test.stroke, diff)
 		}
 	}
 }
